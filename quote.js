8053a937df1d0be889e700aa9ec650b69834b470
Brian Stovall <hoppenmaus@gmail.com>
2016-01-19 21:41:42 -0500

    added quotes. basic functionallity other than tweeting in place. needs some polish

--- quote.js
+++ quote.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var getQuote = document.getElementById('getQuote');
+	var grab = document.getElementById.bind(document);
+	var getQuote = grab('getQuote');
+	var quoteBox  = grab('quoteBox');
+	var authBox = grab('authBox');
+	getQuote.addEventListener('click', () => {
+		var entry = quotes[Math.floor(Math.random() * quotes.length)];
+		quoteBox.textContent = entry.quote;
+		authBox.textContent = entry.author;
+	});
 });
---
+++ quotes.js
@@ -0,0 +1,6 @@
+quotes = [
+		{"quote":"I don't know shit about shit.",'author':'Little Bear'},
+		{"quote":"What the what?!",'author':'Adder Oaks'},
+		{"quote":"Poopy on your face. A deek and dah! blublublubwer",'author':'Finley'},
+		{"quote":"It's... Tuesday.",'author':'Brian'}
+];
