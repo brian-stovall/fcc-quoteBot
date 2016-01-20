174c6b71ab006369411e70b1372e9ac179b395a8
Brian Stovall <hoppenmaus@gmail.com>
2016-01-20 10:21:20 -0500

    added twitter function

--- quotes.js
+++ quotes.js
@@ -2,5 +2,9 @@ quotes = [
 		{"quote":"I don't know shit about shit.",'author':'Little Bear'},
 		{"quote":"What the what?!",'author':'Adder Oaks'},
 		{"quote":"Poopy on your face. A deek and dah! blublublubwer",'author':'Finley'},
+		{"quote":"This is a very long quote to test the response of the " + 
+		          'speech bubble to housing a stupidly long quote... I wonder ' + 
+							'what will happen when it encounters this. Hopefully the ' +
+							'response will be absolutively smashadilliactic!','author':'Annoyus'},
 		{"quote":"It's... Tuesday.",'author':'Brian'}
 ];
