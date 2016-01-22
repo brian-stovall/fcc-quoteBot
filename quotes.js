e1ed4fae69f440dc2c2f2108852cd6abfd2cc7ec
Brian Stovall <hoppenmaus@gmail.com>
2016-01-21 20:53:07 -0500

    added lots of funny quotes and font-awesomeness

--- quotes.js
+++ quotes.js
@@ -1,7 +1,34 @@
 quotes = [
-		{"quote":"I don't know shit about shit.",'author':'Little Bear'},
-		{"quote":"What the what?!",'author':'Adder Oaks'},
-		{"quote":"Poopy on your facex A deek and dah! blublublubwer",'author':'Finley'},
-		{"quote":"this is exactly one hundred and fourty characters xxxxxxxx xxxxxxxxxx xxxxxxxxxxx xxxxxxxxx xx xxxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxx",'author':'Annoyus'},
-		{"quote":"It's... Tuesday.",'author':'Brian'}
+		{"quote":"People who think they know everything are a great " +
+		         'annoyance to those of us who do.','author':'Isaac Asimov'},
+		{"quote":"I can resist everything but temptation.",'author':'Oscar Wilde'},
+		{"quote":"Always remember that you are absolutely unique" +
+						 ' - just like everyone else.','author':'Margaret Mead'},
+		{"quote":"Get your facts first, then you can distort them as" + 
+		         ' you please.','author':'Mark Twain'},
+		{"quote":"Procrastination is the art of keeping up with yesterday.",'author':'Don Marquis'},
+		{"quote":"I love deadlines. I like the wooshing sound they make as they fly by.",
+							'author':'Douglas Adams'},
+		{"quote":"If you could kick the person in the pants responsible for most " +
+							'of your trouble, you wouldn\'t sit for a month.','author':'Theodore Roosevelt'},
+		{"quote":"My fake plants died because I did not pretend to " + 
+						 'water them.','author':'Mitch Hedberg'},
+		{"quote":"If two wrongs don\'t make a right, try three.",'author':'Laurence J. Peter'},
+		{"quote":"Roses are red, violets are blue, I'm schizophrenic " +
+						 'and so am I.','author':'Oscar Levant'},
+		{"quote":"Frisbeetarianism is the belief that when you die " +
+		         'your soul goes up on the roof and gets stuck.','author':'George Carlin'},
+		{"quote":"Reality continues to ruin my life.",'author':'Bill Watterson'},
+		{"quote":"Life is hard. After all, it kills you.",'author':'Katherine Hepburn'},
+		{"quote":"If I were two-faced, would I be wearing this one?",'author':'Abraham Lincoln'},
+		{"quote":"I have six locks on my door all in a row. When I " +
+						 'go out, I lock every other one. I figure no matter how long ' +
+						 'somebody stands there picking locks, they are always locking three.',
+						 'author':'Elayne Boosler'},
+		{"quote":"I was the kid next door's imaginary friend.",'author':'Emo Philips'},
+		{"quote":"Be obscure clearly.",'author':'E.B. White'},
+		{"quote":"Every dogma has its day.",'author':'Anthony Burgess'},
+		{"quote":"If you live to be one hundred, you've got it made. " +
+						 'Very few people die past that age.','author':'George Burns'},
+		{"quote":"I never said most of the things I said.",'author':'Yogi Berra'}
 ];
